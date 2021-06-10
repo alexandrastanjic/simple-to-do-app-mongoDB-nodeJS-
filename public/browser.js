d38377d929af70b65de8ae60200c0a6e2df1fd49
Alexandra S <alexandrastanjic@gmail.com>
2021-06-10 19:12:31 +0300

    security

--- public/browser.js
+++ public/browser.js
@@ -1,51 +1,63 @@
 function itemTemplate(item) {
-  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+	return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
   <span class="item-text">${item.text}</span>
+  <div>
   <div>
     <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
     <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
   </div>
-</li>`
+</li>`;
 }
-let createField = document.getElementById("create-field")
 
-document.getElementById("create-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  axios.post('/create-item', {
-    text: createField.value
-  }).then(function (response) {
-    document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data));
-    createField.value = '';
-    createField.focus();
+let ourHTML = items
+	.map(function(item) {
+		return itemTemplate(item);
+	})
+	.join('');
+document.getElementById('item-list').insertAdjacentHTML('beforeend', ourHTML);
+let createField = document.getElementById('create-field');
 
-  }).catch(function () {
-    console.log("Please try again later.")
-  })
+document.getElementById('create-form').addEventListener('submit', function(e) {
+	e.preventDefault();
+	axios
+		.post('/create-item', {
+			text: createField.value
+		})
+		.then(function(response) {
+			document.getElementById('item-list').insertAdjacentHTML('beforeend', itemTemplate(response.data));
+			createField.value = '';
+			createField.focus();
+		})
+		.catch(function() {
+			console.log('Please try again later.');
+		});
 });
 
+document.addEventListener('click', function(e) {
+	if (e.target.classList.contains('delete-me')) {
+		if (confirm('Do you really want to delete this item permanently?')) {
+			axios
+				.post('/delete-item', {
+					id: e.target.getAttribute('data-id')
+				})
+				.then(function() {
+					e.target.parentElement.parentElement.remove();
+				})
+				.catch(function() {
+					console.log('Please try again later.');
+				});
+		}
+	}
 
-document.addEventListener("click", function (e) {
-  if (e.target.classList.contains("delete-me")) {
-    if (confirm("Do you really want to delete this item permanently?")) {
-      axios.post('/delete-item', {
-        id: e.target.getAttribute("data-id")
-      }).then(function () {
-        e.target.parentElement.parentElement.remove()
-      }).catch(function () {
-        console.log("Please try again later.")
-      })
-    }
-  };
-
-
-  if (e.target.classList.contains("edit-me")) {
-    let userInput = prompt("Enter yout desired new text");
-    axios.post('/update-item', {
-      text: userInput
-    }).then(function () {
-
-    }).catch(function () {
-      console.log("Error")
-    })
-  }
+	if (e.target.classList.contains('edit-me')) {
+		let userInput = prompt('Enter yout desired new text');
+		axios
+			.post('/update-item', {
+				text: userInput
+			})
+			.then(function() {})
+			.catch(function() {
+				console.log('Error');
+			});
+	}
 });
