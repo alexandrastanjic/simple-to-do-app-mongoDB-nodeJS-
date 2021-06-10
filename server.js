92ba9f2209eb0dcd5305a1abed37893648414797
Alexandra S <alexandrastanjic@gmail.com>
2021-06-10 13:09:55 +0300

    CRUD for todo app

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 let express = require('express');
+let mongodb = require('mongodb');
 
 let app = express();
+let db;
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  res.send(`<!DOCTYPE html>
+let connectionString = 'mongodb+srv://todoAppUser:todoAppUser12@cluster0.xiqoc.mongodb.net/TodoApp?retryWrites=true&w=majority';
+mongodb.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, function (err, client) {
+
+  db = client.db('todoAppUser')
+  app.listen(3000);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+
+app.get('/', function (req, res) {
+  db.collection('items').find().toArray(function (err, items) {
+
+    res.send(`<!DOCTYPE html>
   <html>
   <head>
     <meta charset="UTF-8">
@@ -18,46 +37,52 @@ app.get('/', function(req, res){
       <h1 class="display-4 text-center py-1">To-Do App</h1>
       
       <div class="jumbotron p-3 shadow-sm">
-        <form action="/create-item" method="POST">
+        <form id="create-form" action="/create-item" method="POST">
           <div class="d-flex align-items-center">
-            <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+            <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
             <button class="btn btn-primary">Add New Item</button>
           </div>
         </form>
       </div>
       
-      <ul class="list-group pb-5">
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #1</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #2</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-          <span class="item-text">Fake example item #3</span>
-          <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-          </div>
-        </li>
+      <ul id ="item-list" class="list-group pb-5">
+       ${items.map(function(item) {
+         return  `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+         <span class="item-text">${item.text}</span>
+         <div>
+           <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+           <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+         </div>
+       </li>`
+       }).join('')}
      </ul>
-      
+       
     </div>
-    
+
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="/browser.js"></script>
   </body>
   </html>`)
+  });
+
 });
 
-app.post('/create-item', function(req, res){
-  console.log(req.body.item);
-  res.send("Thank for submitting the form")
-})
-app.listen(3000) 
+
+app.post('/create-item', function (req, res) {
+  db.collection('items').insertOne({
+    text: req.body.text
+  }, function (err, info) {
+    res.json(info.ops[0])
+  })
+});
+app.post('/update-item', function(req, res){
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+    res.send("Success")
+  })
+});
+
+app.post('/delete-item', function(req, res){
+  db. collection('items').deleteOne({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: req.body.text}}, function() {
+    res.send("Success")
+  })
+});
